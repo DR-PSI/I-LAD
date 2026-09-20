387099d6bca60b4c7ede41e2345028a06f10f122
DR-PSI <mplusgood@gmail.com>
2026-09-20 19:30:13 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE = 'ticc-v1';
-const ASSETS = ['./IronEye Feeder.dc.html', './manifest.json', './icons/icon-192.png', './icons/icon-512.png'];
+const ASSETS = ['./index.html', './manifest.json', './icons/icon-192.png', './icons/icon-512.png'];
 
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
